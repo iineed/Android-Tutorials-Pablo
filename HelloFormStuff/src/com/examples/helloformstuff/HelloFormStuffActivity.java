0c05e671d3d378902e43fafe6e9018608071fae2
Pablo <pabposta@gmail.com>
2011-08-16 22:04:42 +0200

    First commit

---
+++ HelloFormStuff/src/com/examples/helloformstuff/HelloFormStuffActivity.java
@@ -0,0 +1,23 @@
+package com.examples.helloformstuff;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class HelloFormStuffActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on clicks
+                Toast.makeText(HelloFormStuffActivity.this, "Beep Bop", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
